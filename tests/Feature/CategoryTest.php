1ea2fd31b0107242a2e096b5fdc2c8a4ad1d7b8d
Sagit Gutierrez <jozaguts@gmail.com>
2022-03-22 22:26:25 -0600

    init passport

--- tests/Feature/CategoryTest.php
+++ tests/Feature/CategoryTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -15,9 +16,15 @@ class CategoryTest extends TestCase
      */
     public function test_example()
     {
+        $user = User::factory()->create();
 
-        $response = $this->get('/categories');
+        $response = $this->actingAs($user, 'api')
+            ->get('/api/v1/categories');
 
-        $response->assertStatus(200);
+        $response
+            ->assertStatus(200)
+            ->assertJson([
+                'data' => [],
+            ]);
     }
 }
