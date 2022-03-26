1d553b125fa1038c3023bacbb0551b51dd6eb1ca
Sagit Gutierrez <jozaguts@gmail.com>
2022-03-25 21:09:59 -0600

    auth-api authentication

---
+++ app/Http/Requests/JsonApiAuth/PasswordResetLinkRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests\JsonApiAuth;
+
+use App\Models\User;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class PasswordResetLinkRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => ['required', 'email', Rule::exists(User::class)],
+        ];
+    }
+
+    public function getUser()
+    {
+        $email = $this->get('email');
+
+        return User::query()->where('email', $email)->first();
+    }
+}
