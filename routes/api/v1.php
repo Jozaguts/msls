6e2cfa92550ed410bf8e8c448d8ea13e2e081a74
Sagit Gutierrez <jozaguts@gmail.com>
2022-03-26 15:29:08 -0600

    hard-delete

--- routes/api/v1.php
+++ routes/api/v1.php
@@ -2,11 +2,13 @@
 
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\GameController;
+use App\Http\Controllers\GenderController;
 use App\Http\Controllers\PenaltyController;
 use App\Http\Controllers\PenaltyGoalKeeperController;
 use App\Http\Controllers\PlayersController;
 use App\Http\Controllers\RefereeController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\TeamsController;
 
@@ -20,7 +22,23 @@ use \App\Http\Controllers\TeamsController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::middleware(['auth:api'])->group(function(){
+require __DIR__ . '/../json-api-auth.php';
+
+Route::middleware(['auth:api','verified'])->group(function(){
+    Route::post('/hard-delete', function() {
+        DB::beginTransaction();
+        try{
+            DB::table(request()->input('model'))
+                ->delete(request()->input('id'));
+            DB::commit();
+             return response()->json(['data'=> ['message' => 'The register was hard deleted successfully']]);
+        }catch(Exception $e){
+            DB::rollBack();
+            return response()->json( $e->getMessage(),400);
+        }
+
+    });
+    Route::apiResources(['genders'=> GenderController::class]);
     Route::apiResources([
         'teams' => TeamsController::class
     ]);
@@ -46,3 +64,4 @@ Route::middleware(['auth:api'])->group(function(){
         'games' => GameController::class
     ]);
 });
+
