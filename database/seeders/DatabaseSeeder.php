6a5596f4259f116095d36be227f080d463af3e26
Sagit Gutierrez <jozaguts@gmail.com>
2022-03-27 15:25:17 -0600

    user admin@sls.com added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,7 @@ use App\Models\Player;
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,7 +17,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-//         \App\Models\User::factory(10)->create();
+        //todo refactor user no necesita name
+         $user = new \App\Models\User();
+            $user->name = 'Jose Sagit Gutierrez';
+            $user->email = 'admin@sls.com';
+            $user->password = Hash::make('password');
+            $user->email_verified_at = now();
+         $user->save();
         $this->call(PositionsTableSeeder::class);
         $this->call(GendersTableSeeder::class);
         $this->call(CategoriesTableSeeder::class);
